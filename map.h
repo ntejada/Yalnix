46ea67a2970e0b0d2e7258ddeed82f41ee565598
Garrett S. Watumull 16 <gwatumull@kearsarge.cs.dartmouth.edu>
2014-10-20 21:36:31 -0400

    map.c map.h

---
+++ map.h
@@ -0,0 +1,31 @@
+#ifndef _map_h
+#define _map_h
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define HASH_SIZE 100
+
+typedef struct _MapData MapData;
+
+MapData *hash_table[HASH_SIZE];
+
+
+
+struct _MapData {
+  MapData *next;
+  int pid;
+  void *kernelsp_1;
+  void *kernelsp_2;
+};
+
+int hashInit(MapData **);
+int hash(int);
+MapData *accessMap(int);
+int insertMap(MapData *);
+int removeFromMap(int);
+MapData *createMapData(int, void *, void*);
+
+#endif 
+
+
