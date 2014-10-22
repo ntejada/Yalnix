f901fedf732df173ba63642fa36536f0025e69ed
Shuo Zheng <szheng@tahoe.cs.dartmouth.edu>
2014-10-22 19:55:14 -0400

    changes in gitignore, and CopyPages()

--- switch.c
+++ switch.c
@@ -20,11 +20,12 @@ KernelContext *MyKCS(KernelContext *kc_in, void *p_curr_pcb, void *p_next_pcb)
     if((next->status) == NEW){
       TracePrintf(1, "next given kernel context\n");
       next->kernel_context = *kc_in;
+      next->status = RUNNING;
     } 
 
     pZeroTable[KERNEL_STACK_BASE>>PAGESHIFT].pfn = next->kStackPages[0]; 
     pZeroTable[(KERNEL_STACK_BASE>>PAGESHIFT)+1].pfn = next->kStackPages[1];
-    
+
     WriteRegister(REG_TLB_FLUSH, TLB_FLUSH_ALL);
 
     return &(next->kernel_context);
