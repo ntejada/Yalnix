aedd7cf27eeb8e883faea491abf3a61d4e4d0c87
Nicolas S. Tejada <ntejada@cs.dartmouth.edu>
2014-10-24 16:01:31 -0400

    fixed brkTest.c

--- brkTest.c
+++ brkTest.c
@@ -1,12 +1,9 @@
 int main(int argc, char*argv[]){
-	TracePrintf(1, "in brkTest\n");
-	int pid;
 	int i = 10;
-	void *rc;
-	while (i-->0) {
-		pid = GetPid();
-		TracePrintf(1, "Hello World I am process %d\n", pid);
-		rc = (void *)malloc(4000);
+	void * rc;
+	while (i--) {
+		TracePrintf(1, "in %s\n", argv[1]);
+		rc = malloc(4000);	
 		Pause(1);
 	}
 	free(rc);
