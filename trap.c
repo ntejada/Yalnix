b6e38412ae1f24dac797c7b92d343ba1b7d31461
Shuo Zheng <shuo.zheng@outlook.com>
2014-10-30 14:13:19 -0400

    implemented copy on write

--- trap.c
+++ trap.c
@@ -15,6 +15,7 @@
 #include "util/list.h"
 #include "std.h"
 #include "frames.h"
+#include "init.h"
 
 void *trapVector[TRAP_VECTOR_SIZE];
 
@@ -38,70 +39,70 @@ void InitTrapVector() {
 void KernelCallHandler(UserContext *context) {
     int rc;
     switch (context->code) {
-    case YALNIX_FORK:
-        DoFork(context);
-        break;
-    case YALNIX_EXEC:
-        DoExec(context);
-        break;
-    case YALNIX_EXIT:
-        DoExit(context);
-        break;
-    case YALNIX_WAIT:
-        DoWait(context);
-        break;
-    case YALNIX_GETPID:
-        DoGetPid(context);
-        break;
-    case YALNIX_BRK:
-        DoBrk(context);
-        break;
-    case YALNIX_DELAY:
-        DoDelay(context);
-        break;
-	/*
-    case YALNIX_TTY_READ:
-        doTtyRead(context);
-        break;
-    case YALNIX_TTY_WRITE:
-        doTtyWrite(context);
-        break;
+        case YALNIX_FORK:
+            DoFork(context);
+            break;
+        case YALNIX_EXEC:
+            DoExec(context);
+            break;
+        case YALNIX_EXIT:
+            DoExit(context);
+            break;
+        case YALNIX_WAIT:
+            DoWait(context);
+            break;
+        case YALNIX_GETPID:
+            DoGetPid(context);
+            break;
+        case YALNIX_BRK:
+            DoBrk(context);
+            break;
+        case YALNIX_DELAY:
+            DoDelay(context);
+            break;
+            /*
+               case YALNIX_TTY_READ:
+               doTtyRead(context);
+               break;
+               case YALNIX_TTY_WRITE:
+               doTtyWrite(context);
+               break;
 #ifdef LINUX
-    case YALNIX_PIPE_INIT:
-        doPipeInit(context);
-        break;
-    case YALNIX_PIPE_READ:
-        doPipeRead(context);
-        break;
-    case YALNIX_PIPE_WRITE:
-        doPipeWrite(context);
-        break;
-    case YALNIX_LOCK_INIT:
-        doLockInit(context);
-        break;
-    case YALNIX_LOCK_ACQUIRE:
-        doLockAcquire(context);
-        break;
-    case YALNIX_LOCK_RELEASE:
-        doLockRelease(context);
-        break;
-    case YALNIX_CVAR_INIT:
-        doCvarWait(context);
-        break;
-    case YALNIX_CVAR_SIGNAL:
-        doCvarSignal(context);
-        break;
-    case YALNIX_CVAR_BROADCAST:
-        doCvarBroadcast(context);
-        break;
-    case YALNIX_CVAR_WAIT:
-        doCvarWait(context);
-        break;
-    case YALNIX_RECLAIM:
-        doReclaim(context);
-        break;
+case YALNIX_PIPE_INIT:
+doPipeInit(context);
+break;
+case YALNIX_PIPE_READ:
+doPipeRead(context);
+break;
+case YALNIX_PIPE_WRITE:
+doPipeWrite(context);
+break;
+case YALNIX_LOCK_INIT:
+doLockInit(context);
+break;
+case YALNIX_LOCK_ACQUIRE:
+doLockAcquire(context);
+break;
+case YALNIX_LOCK_RELEASE:
+doLockRelease(context);
+break;
+case YALNIX_CVAR_INIT:
+doCvarWait(context);
+break;
+case YALNIX_CVAR_SIGNAL:
+doCvarSignal(context);
+break;
+case YALNIX_CVAR_BROADCAST:
+doCvarBroadcast(context);
+break;
+case YALNIX_CVAR_WAIT:
+doCvarWait(context);
+break;
+case YALNIX_RECLAIM:
+doReclaim(context);
+break;
 #endif // LINUX
-	*/
+*/
     }
 }
 
@@ -127,37 +128,63 @@ void MathHandler(UserContext *context) {
 
 void MemoryHandler(UserContext *context) {
     TracePrintf(1, "Trap Memory\n");
-    int newStackPage = (DOWN_TO_PAGE((int)context->addr - VMEM_1_BASE)>>PAGESHIFT);
+    int pageNum = (DOWN_TO_PAGE((int)context->addr - VMEM_1_BASE)>>PAGESHIFT);
     int sp = DOWN_TO_PAGE(current_process->user_context.sp - VMEM_1_BASE)>>PAGESHIFT;
-    
-    TracePrintf(2, "TrapMemory: New stack page = %d. Old stack = %d\n", newStackPage, sp);	
+
+    TracePrintf(2, "TrapMemory: New stack page = %d. Old stack = %d\n", pageNum, sp);	
     TracePrintf(2, "TrapMemory: Context addr: %p\n", context->addr);
     switch (context->code) {
-    case YALNIX_MAPERR:
+        case YALNIX_MAPERR:
 
-	if (current_process->cowPageTable.pageTable[newStackPage - 1].valid == 1) {
-	    TracePrintf(1, "Memory Error: Attempt to extend stack too close to heap\n");
+            if (current_process->cow.pageTable[pageNum - 1].valid == 1) {
+                TracePrintf(1, "Memory Error: Attempt to extend stack too close to heap\n");
 
-	    current_process->status = KILL;
-	    KillProc(current_process);
-	    LoadNextProc(context, BLOCK);
-	}
+                current_process->status = KILL;
+                KillProc(current_process);
+                LoadNextProc(context, BLOCK);
+            }
 
-	for (sp; sp <= newStackPage; sp++) {
-	    current_process->cowPageTable.pageTable[newStackPage].valid = 1;
-	    current_process->cowPageTable.pageTable[newStackPage].pfn = getNextFrame();
-	    current_process->cowPageTable.pageTable[newStackPage].prot = (PROT_READ | PROT_WRITE);
-	}
-        break;
-    case YALNIX_ACCERR:
-	TracePrintf(1, "Memory Error: Tried to access page without correct permissions\n");
-        TracePrintf(1, "Memory Error: Killing Current Process\n");
+            for (sp; sp <= pageNum; sp++) {
+                current_process->cow.pageTable[pageNum].valid = 1;
+                current_process->cow.pageTable[pageNum].pfn = getNextFrame();
+                current_process->cow.pageTable[pageNum].prot = (PROT_READ | PROT_WRITE);
+            }
+            break;
+        case YALNIX_ACCERR:
+            // Check first to see if it was a copy on write issue, ie. attempt to write to a CoW page
+            if (current_process->cow.refCount[pageNum] && 
+                    current_process->cow.pageTable[pageNum].prot == PROT_READ) {
+                // Go ahead and change permissions
+                current_process->cow.pageTable[pageNum].prot = (PROT_READ|PROT_WRITE);
+                // Check how many processes are referencing that frame
+                if (current_process->cow.refCount[pageNum] > 1) {
+                    // Copy over the page and decrement refCount
+                    pZeroTable[PF_COPIER].valid = 1;
+                    pZeroTable[PF_COPIER].prot = (PROT_READ|PROT_WRITE);
 
-        current_process->status = KILL;
-        KillProc(current_process);
-        LoadNextProc(context, BLOCK);
+                    int newPfn = getNextFrame();
+                    pZeroTable[PF_COPIER].pfn = newPfn;
+                    WriteRegister(REG_TLB_FLUSH, PF_COPIER << PAGESHIFT);
+                    memcpy(PF_COPIER << PAGESHIFT, VMEM_1_BASE + (pageNum << PAGESHIFT), PAGESIZE);
+                    current_process->cow.pageTable[pageNum].pfn = newPfn;
+                    *(current_process->cow.refCount[pageNum])--;
 
-        break;
+                    pZeroTable[PF_COPIER].prot = PROT_NONE;
+                    pZeroTable[PF_COPIER].valid = 0;
+                } else {
+                    free(current_process->cow.refCount[pageNum]);
+                }
+            } else {
+                // Otherwise it was truly a bad memory access.
+                TracePrintf(1, "Memory Error: Tried to access page without correct permissions\n");
+                TracePrintf(1, "Memory Error: Killing Current Process\n");
+
+                current_process->status = KILL;
+                KillProc(current_process);
+                LoadNextProc(context, BLOCK);
+            }
+
+            break;
     }
 }
 
@@ -175,7 +202,7 @@ void DiskHandler(UserContext *context) {
 void InvalidTrapHandler(UserContext *context) {
     context->regs[0] = ERROR;
     TracePrintf(1, "Invalid trap occurred from process %d with trap code %d\n",
-                current_process->id, context->vector);
+            current_process->id, context->vector);
     KillProc(current_process);
     LoadNextProc(context, BLOCK);
 
